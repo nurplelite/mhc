755453949b163cc40bcf1bf6417e8741150f5d24
AYH <dev@madhareconsulting.com>
2025-02-09 09:16:00 +0000

    initialized again

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-nav',
+  imports: [RouterModule, MatButtonModule, MatIconModule, MatToolbarModule, MatMenuModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+ isCollapsed = true;
+}
