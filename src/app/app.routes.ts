e9b31a77cc6fa9086acb4b21326c7dacadffc6a2
AYH <dev@madhareconsulting.com>
2025-02-09 14:09:04 +0000

    update master slow ssh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'services', component: ServicesComponent}
+];
