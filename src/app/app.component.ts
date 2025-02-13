caee237cfcc0c06b03e38ebf80c65a124c490cf0
nurplelite <dev@madhareconsulting.com>
2025-02-13 13:25:15 -0500

    added material side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./components/nav/nav.component";
 import { ThemeService } from './theme.service';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { LayoutComponent } from './core/layout/layout.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'gallery', component: GalleryComponent },
-    { path: 'services', component: ServicesComponent}
+    { path: '', 
+        component: LayoutComponent, pathMatch: 'full',
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'about', component: AboutComponent },
+            { path: 'gallery', component: GalleryComponent },
+            { path: 'services', component: ServicesComponent}
+        ]
+    }
 ];
